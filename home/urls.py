4bf2f719f1bdf013f283b514b54d8dc54e39e4fd
khacluat03 <khacluat1123@gmail.com>
2025-05-26 05:20:35 +0700

    xoa het

--- home/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import index, preregister
-
-urlpatterns = [
-    path('', preregister, name='preregister'),
-    path('home/', index, name="index"),
-]
--- search/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from search.views import search
-
-urlpatterns = [
-    path('', search, name="search"),
-]
